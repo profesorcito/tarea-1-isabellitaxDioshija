758265613bfbf31700ccdf2e93fcf44f16c584da
isabellitaxDioshija <isabellagarzon2006@javerianacali.edu.co>
2025-02-09 15:16:33 -0500

    Intento 8

--- EstacionamientoIntergalactico2/main.cpp
+++ EstacionamientoIntergalactico2/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 // TODO: Implementar función para calcular la suma de los dígitos de un número
 int sumaDigitos(int n) {
-    int sumarDig = 0;
+    int sumar = 0;
     while (n > 0) {
-        sumarDig += n % 10;
+        sumar += n % 10;
         n /= 10;
     }
-    return sumarDig;
+    return sumar;
 }
 
 // TODO: Implementar la lógica para calcular la tarifa
