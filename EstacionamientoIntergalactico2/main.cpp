258583cc61123732de808195cd8a308e4ef9034a
isabellitaxDioshija <isabellagarzon2006@javerianacali.edu.co>
2025-02-09 14:52:50 -0500

    Intento 7

--- EstacionamientoIntergalactico2/main.cpp
+++ EstacionamientoIntergalactico2/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Función para calcular la suma de los dígitos de un número
+// TODO: Implementar función para calcular la suma de los dígitos de un número
 int sumaDigitos(int n) {
     int sumarDig = 0;
     while (n > 0) {
@@ -12,7 +12,7 @@ int sumaDigitos(int n) {
     return sumarDig;
 }
 
-// Función para calcular la tarifa
+// TODO: Implementar la lógica para calcular la tarifa
 double calcularTarifa(int horas, int dia) {
     const double TARIFA_PRIMERA_HORA = 6.00;
     const double TARIFA_1_A_3 = 4.00;
@@ -29,7 +29,7 @@ double calcularTarifa(int horas, int dia) {
 
     double totalPagar = 0.0;
 
-    // Cálculo de la tarifa base
+    // TODO: Implementar la lógica para calcular la tarifa base
     if (horas == 1) {
         totalPagar = TARIFA_PRIMERA_HORA;
     } else if (horas > 1 && horas <= 3) {
@@ -40,12 +40,12 @@ double calcularTarifa(int horas, int dia) {
         totalPagar = TARIFA_FIJA;
     }
 
-    // Aplicar incremento si es fin de semana
+    // TODO: Aplicar incremento si es fin de semana
     if (dia == 6 || dia == 7) {
         totalPagar *= INCREMENTO_FIN_SEMANA;
     }
 
-    // Aplicar descuento si la suma de los dígitos de las horas es múltiplo de 3
+    // TODO: Aplicar descuento si la suma de los dígitos es múltiplo de 3
     if (sumaDigitos(horas) % 3 == 0) {
         totalPagar *= DESCUENTO_DIGITOS;
     }
@@ -68,8 +68,6 @@ int main() {
 
     return 0;
 }
-
-
 
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
